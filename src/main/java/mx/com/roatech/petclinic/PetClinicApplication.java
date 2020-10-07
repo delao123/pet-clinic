17d893dd3c2e6216b2bbdfbb0dee387f4c34b42a
De la O <carlosdelao1234@gmail.com>
2020-10-07 10:32:57 -0500

    Initial Project Add

---
+++ src/main/java/mx/com/roatech/petclinic/PetClinicApplication.java
@@ -0,0 +1,13 @@
+package mx.com.roatech.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicApplication.class, args);
+    }
+
+}
