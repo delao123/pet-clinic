b5a7ebf9ee5b4cb2cefe2d206fa80d8f390d487e
Carlos De la O <juan.olopez@roatech.com.mx>
2020-10-15 15:11:04 -0500

    Add Base Entity Class

--- pet-clinic-data/src/main/java/mx/com/roatech/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/mx/com/roatech/petclinic/model/Person.java
@@ -1,6 +1,6 @@
 package mx.com.roatech.petclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/mx/com/roatech/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mx/com/roatech/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package mx.com.roatech.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
