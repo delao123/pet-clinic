ebcd390ff3a5205ec778273aa2e45cb1adb98353
Carlos De la O <juan.olopez@roatech.com.mx>
2020-10-20 13:33:35 -0500

    Implemented Map Based Services, Closes #5

--- pet-clinic-data/src/main/java/mx/com/roatech/petclinic/services/CrudService.java
+++ pet-clinic-data/src/main/java/mx/com/roatech/petclinic/services/CrudService.java
@@ -6,7 +6,6 @@ public interface CrudService<T,ID> {
     Set<T> findAll();
     T findById(ID id);
     T save(T object);
-
-    void delete(ID id);
-
+    void deleteById(ID id);
+    void delete(T object);
 }
