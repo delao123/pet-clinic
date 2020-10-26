92d4e3a3c93f71e62ba9b974f9d9c368e82eee65
Carlos De la O <juan.olopez@roatech.com.mx>
2020-10-26 12:09:11 -0600

    List all owners. Closes #13

--- pet-clinic-web/src/main/java/mx/com/roatech/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mx/com/roatech/petclinic/bootstrap/DataLoader.java
@@ -4,10 +4,10 @@ import mx.com.roatech.petclinic.map.OwnerMapService;
 import mx.com.roatech.petclinic.map.VetMapService;
 import mx.com.roatech.petclinic.model.Owner;
 import mx.com.roatech.petclinic.model.Vet;
+import mx.com.roatech.petclinic.services.OwnerService;
 import mx.com.roatech.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import mx.com.roatech.petclinic.services.OwnerService;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -30,7 +30,7 @@ public class DataLoader implements CommandLineRunner {
         Owner owner2 = new Owner();
         owner2.setId(2L);
         owner2.setFirstName("Brianda Paola");
-        owner1.setLastName("Rodriguez Mora");
+        owner2.setLastName("Rodriguez Mora");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
--- pet-clinic-web/src/main/java/mx/com/roatech/petclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/mx/com/roatech/petclinic/controller/OwnerController.java
@@ -1,13 +1,22 @@
 package mx.com.roatech.petclinic.controller;
 
+import mx.com.roatech.petclinic.map.OwnerMapService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
+    private final OwnerMapService ownerMapService;
+
+    public OwnerController(OwnerMapService ownerMapService) {
+        this.ownerMapService = ownerMapService;
+    }
+
     @RequestMapping({"","/index","/index.html"})
-    public String listOwners(){
+    public String listOwners(Model model){
+        model.addAttribute("owners",ownerMapService.findAll());
         return "owners/index";
     }
 }
