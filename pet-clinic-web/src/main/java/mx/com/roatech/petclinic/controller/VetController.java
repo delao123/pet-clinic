22737ff00da3f4a5cd12cf2f2ad454c5bec5e237
Carlos De la O <juan.olopez@roatech.com.mx>
2020-10-26 12:21:15 -0600

    List all vets. Closes #12

--- pet-clinic-web/src/main/java/mx/com/roatech/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/mx/com/roatech/petclinic/controller/VetController.java
@@ -1,12 +1,20 @@
 package mx.com.roatech.petclinic.controller;
 
+import mx.com.roatech.petclinic.map.VetMapService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
+    private final VetMapService vetMapService;
+
+    public VetController(VetMapService vetMapService){
+        this.vetMapService = vetMapService;
+    }
     @RequestMapping({"/vets","/vets/index","/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetMapService.findAll());
         return("vets/index");
     }
 }
